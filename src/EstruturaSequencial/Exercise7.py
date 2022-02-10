63c2b81ff0f1b26870c9b4b01cb8b24fab76c2cd
OhMariaaaz <mgomes741741@gmail.com>
2022-02-09 22:39:12 -0300

    update

--- src/EstruturaSequencial/Exercise7.py
+++ src/EstruturaSequencial/Exercise7.py
@@ -2,7 +2,7 @@
 # em seguida mostre o dobro desta área para o usuário.
 
 lado = float(input('Digite o tamanho do lado do quadrado em centimetros: '))
-area = lado*2
+area = lado**2
 print('A área do quadrado é ', area, 'centimetros.')
 area = area*2
 print('O dobro da área é ', area, 'centimetros.')
